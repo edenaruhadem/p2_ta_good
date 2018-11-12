d58444086faef6c0abde0b228675d82b3fb356d0
EdenaRuh <diego.rios.ctag@gmail.com>
2018-11-12 23:31:21 +0100

    update

--- MaxTimesBigramAPartII.java
+++ MaxTimesBigramAPartII.java
@@ -25,14 +25,14 @@ public class MaxTimesBigramAPartII
 {
     public static class MaxTimesBigramAMapper extends Mapper<Object, Text, Text, Text>
     {
-        private Text year = new Text();       
+        private Text year = new Text();      
         private Text bitimes = new Text();
     HashMap<String, String> mapUnigram =  new HashMap<String, String>();
     //Path unigramFile = new Path("/output/part-r-00000");
     //Aqui leer archivo y rellenar
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{                   
-        Path unigramFile = new Path("/diego/output/part-r-00000");
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{                  
+        Path unigramFile = new Path("/outputPartII/part-r-00000");
         FileSystem fs = FileSystem.get(context.getConfiguration());
         BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(unigramFile)));
         try {
@@ -43,7 +43,7 @@ public class MaxTimesBigramAPartII
         String unigram = parts[1];
         //System.out.println(dec+unigram);
         mapUnigram.put(dec,unigram);
-             do{  
+             do{ 
                line = br.readLine();
         //System.out.println("eeeeeeeeeeeeeeeeeeooooooooooooooooooo"+line);
         if(line!=null){
@@ -52,56 +52,88 @@ public class MaxTimesBigramAPartII
         unigram = parts[1];
         mapUnigram.put(dec,unigram);}
              }while(line != null);
-            } finally { 
+            } finally {
               br.close();
               }
         String [] partsLine = value.toString().trim().split("\t");
-        String bigram = partsLine[0];       
+        String bigram = partsLine[0];      
         String stryear = partsLine[1];
         String times = partsLine[2];
-        System.out.println(bigram+stryear+times);
+        //System.out.println(bigram+stryear+times);
         Integer y = Integer.parseInt(stryear);
         if(y>=1900)
         {
         Integer lastnumy = Integer.parseInt(stryear.substring(stryear.length()-1));
-        Integer decade = y + (10-lastnumy);
+    Integer decade = y - lastnumy;
+        //Integer decade = y + (10-lastnumy);
         String strdecade = Integer.toString(decade);
         Map.Entry<String,String> unigramEntry = getUnigramEntry(mapUnigram, strdecade);
-        String twouni = unigramEntry.getValue().substring(0,1);//De ese unigram extraer las dos primeras letras
-        String bi = bigram.substring(0,1);
-        if(bi.equals(twouni))
-        {
+    Integer sizeUni = unigramEntry.getValue().length();
+    if(sizeUni>1)
+    {
+         String twouni = unigramEntry.getValue().substring(0,1);
+         String [] splitBigram = bigram.split(" ");
+         String bi = splitBigram[0].substring(0,1);
+        if(bi.equals(twouni)){
+            String wordBigram = splitBigram[1];
             year.set(strdecade);
-            bitimes.set(bigram+"/"+times);
-            context.write(year, bitimes);           
-        }               
-        }                         
+                    bitimes.set(wordBigram+"/"+times);
+                    context.write(new Text(year), new Text(bitimes));
+        }
+    }
+    else
+        {
+        String oneuni = unigramEntry.getValue();
+        String [] splitBigram = bigram.split(" ");
+        String uni = splitBigram[0];
+        if(uni.equals(oneuni)){
+            String wordBigram = splitBigram[1];
+            year.set(strdecade);
+                    bitimes.set(wordBigram+"@"+times);
+                    context.write(new Text(year), new Text(bitimes));
+        }       
+    }
+        //String twouni = unigramEntry.getValue().substring(0,1);//De ese unigram extraer las dos primeras letras
+        //String bi = bigram.substring(0,1);
+        //if(bi.equals(twouni))
+        //{
+            //year.set(strdecade);
+            //bitimes.set(bigram+"/"+times);
+            //context.write(year, bitimes);          
+        //}              
+        }                        
         }
     }
 
     public static class MaxTimesBigramAReducer extends Reducer<Text, Text, Text, Text>
     {
-       
+      
         private Text bigram = new Text();
         HashMap<String, Integer> map =  new HashMap<String, Integer>();
-       
+      
         public void reduce(Text key, Iterable<Text> values, Context context)throws IOException, InterruptedException {
 
-            Integer maxValue = 0;           
+            Integer maxValue = 0;          
             for (Text val : values)
             {
-                String[] parts = val.toString().split("/");
+                String[] parts = val.toString().split("@");
                 String bi = parts[0];
-        String t = parts[1];       
-                Integer time = Integer.parseInt(t);
-        //System.out.println(uni+"/"+t);
-                map.put(bi, time);               
+            String t = parts[1];
+       
+        if(t.indexOf(".")==-1){      
+                Integer time = Integer.parseInt(t);       
+                map.put(bi, time);}
+
+      
+                /*Integer time = Integer.parseInt(t);
+       
+                map.put(bi, time);*/              
             }
             Map.Entry<String,Integer> maxEntry =  getMaxEntry(map);
-        String bi = maxEntry.getKey();                  
+        String bi = maxEntry.getKey();                 
             bigram.set(bi);
         map.clear();
-            context.write(key, bigram);           
+            context.write(key, bigram);          
         }
     }
 
@@ -123,14 +155,14 @@ public class MaxTimesBigramAPartII
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-       
+      
     System.exit(job.waitForCompletion(true) ? 0 : 1);
       }
-  
+ 
 
-    public static Entry<String, Integer> getMaxEntry(HashMap<String, Integer> map){      
+    public static Entry<String, Integer> getMaxEntry(HashMap<String, Integer> map){     
         Entry<String, Integer> maxEntry = null;
-        Integer max = Collections.max(map.values());  
+        Integer max = Collections.max(map.values()); 
         for(Entry<String, Integer> entry : map.entrySet()) {
             Integer value = entry.getValue();
             if(null != value && max == value) {
@@ -140,9 +172,9 @@ public class MaxTimesBigramAPartII
         return maxEntry;
     }
 
-    public static Entry<String, String> getUnigramEntry(HashMap<String, String> mapUnigram, String decade){      
+    public static Entry<String, String> getUnigramEntry(HashMap<String, String> mapUnigram, String decade){     
         Entry<String, String> unigramEntry = null;
-        //Integer max = Collections.max(map.values());  
+        //Integer max = Collections.max(map.values()); 
         for(Entry<String, String> entry : mapUnigram.entrySet()) {
             String key = entry.getKey();
             if(decade.equals(key)) {
@@ -154,10 +186,14 @@ public class MaxTimesBigramAPartII
 
 }
 
-   
-   
-   
 
+  
+  
+  
+
+   
+   
+   
 	
 	
 	
--- MaxTimesUnigramAPartI.java
+++ MaxTimesUnigramAPartI.java
@@ -22,58 +22,73 @@ public class MaxTimesUnigramAPartI
 {
     public static class MaxTimesUnigramAMapper extends Mapper<Object, Text, Text, Text>
     {
-        private Text year = new Text();       
+        private Text year = new Text();      
         private Text unitimes = new Text();
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{                   
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+    //System.out.println(value.toString());                  
         String [] partsLine = value.toString().trim().split("\t");
-        String unigram = partsLine[0];       
+        String unigram = partsLine[0];      
         String stryear = partsLine[1];
         String times = partsLine[2];
+    /*if(times.indexOf("_")!=-1)
+    {
+        String [] partsTime = times.split("_");
+        times = partsTime[0];       
+    }*/
         Integer y = Integer.parseInt(stryear);
         if(y>=1800)
         {
         Integer lastnumy = Integer.parseInt(stryear.substring(stryear.length()-1));
-        Integer decade = y + (10-lastnumy);
-        String strdecade = Integer.toString(decade);   
+        //Integer decade = y + (10-lastnumy);
+    Integer decade = y - lastnumy;
+        String strdecade = Integer.toString(decade);  
         year.set(strdecade);
-        unitimes.set(unigram+"/"+times);
-        context.write(year, unitimes);       
+        unitimes.set(unigram+"@"+times);
+    //System.out.println(unigram+"/"+times);
+        context.write(new Text(year), new Text(unitimes));      
         }
-                         
+                        
         }
     }
-
-
-   
-
-
-
-
-
     public static class MaxTimesUnigramAReducer extends Reducer<Text, Text, Text, Text>
     {
-       
+      
         private Text unigram = new Text();
         HashMap<String, Integer> map =  new HashMap<String, Integer>();
-       
+      
         public void reduce(Text key, Iterable<Text> values, Context context)throws IOException, InterruptedException {
 
-            Integer maxValue = 0;           
+            Integer maxValue = 0;          
             for (Text val : values)
             {
-                String[] parts = val.toString().split("/");
+        //System.out.println(val.toString());
+                String[] parts = val.toString().trim().split("@");
                 String uni = parts[0];
-        String t = parts[1];       
-                Integer time = Integer.parseInt(t);
-        //System.out.println(uni+"/"+t);
-                map.put(uni, time);               
+                String t = parts[1];
+
+        /*if(t.indexOf("_")!=-1)
+                {
+           String [] partst = t.split("_");
+               t = partst[0];       
+            }*/
+
+        //System.out.println(uni+"@"+t);
+        //String prueba = "editorial.ucr.ac.cr";
+        if(t.indexOf(".")==-1){      
+                Integer time = Integer.parseInt(t);       
+                map.put(uni, time);}              
             }
             Map.Entry<String,Integer> maxEntry =  getMaxEntry(map);
-        String uni = maxEntry.getKey();                  
-            unigram.set(uni);
-        map.clear();
-            context.write(key, unigram);           
+            String uni = maxEntry.getKey();
+        if(uni.indexOf("_")!=-1)
+                {
+           String [] partsuni = uni.split("_");
+               uni = partsuni[0];       
+            }                         
+            unigram.set(uni);       
+            map.clear();
+            context.write(new Text(key), new Text(unigram));          
         }
     }
 
@@ -91,14 +106,14 @@ public class MaxTimesUnigramAPartI
         job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));               
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));              
         System.exit(job.waitForCompletion(true) ? 0 : 1);
       }
-  
+ 
 
-    public static Entry<String, Integer> getMaxEntry(HashMap<String, Integer> map){      
+    public static Entry<String, Integer> getMaxEntry(HashMap<String, Integer> map){     
         Entry<String, Integer> maxEntry = null;
-        Integer max = Collections.max(map.values());  
+        Integer max = Collections.max(map.values()); 
         for(Entry<String, Integer> entry : map.entrySet()) {
             Integer value = entry.getValue();
             if(null != value && max == value) {
@@ -109,11 +124,3 @@ public class MaxTimesUnigramAPartI
     }
 
 }
-
-   
-   
-   
-
-	
-	
-	
